99bd217fe785b0d7188817048cc102d05c55f311
Rajat Arora <ra_aro@live.concordia.ca>
2021-02-16 00:06:45 -0500

    SR: Added basic structure of the Player Cpp

---
+++ Part3/Player.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Player.h"
+#include "../Part1/Map.h"
+
+
+bool Player :: PayCoin(int coins)
+{
+
+}
+bool Player :: PLaceNewArmies()
+{
+    
+}
+bool Player :: MoveArmies()
+{
+    
+}
+bool Player :: MoveOverLand()
+{
+    
+}
+bool Player :: DestroyArmy()
+{
+    
+}
---
+++ Part3/Player.h
@@ -0,0 +1,13 @@
+#include "../Part1/Map.h"
+#include "../Part4/Cards.h"
+
+class Player
+{
+    bool PayCoin(int coins);
+    bool PLaceNewArmies();
+    bool MoveArmies();
+    bool MoveOverLand();
+    bool BuildCity();
+    bool DestroyArmy();
+
+};
