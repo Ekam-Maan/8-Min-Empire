7db56bd8af8dffcc8174b02628450b413d070c57
Roshleen Mand <45768910+roshleenmand@users.noreply.github.com>
2021-02-21 20:13:26 -0500

    pushing the final code

--- Part5/bidding.cpp
+++ Part5/bidding.cpp
@@ -1,80 +1,109 @@
 #include <iostream>
-#include "player.cpp"
-#include "main.cpp"
+#include "player.h"
 #include <string>
 #include <algorithm>
-#include "bidding.h"
-std nameComparison ();
-
+using namespace std;
 class bidding
 {
-public:			// Access specifier
-  player p1;
-  player p2;
-  player p3;
-  player p4;
-  player p5;
-  player winningPlayer;		// Attributes
+	// Access specifier
+public:
+	player *winningPlayer;	// Attribute
+	player** playerObj= new player *[5];
+	bidding()
+	{}
 
-    bidding (player * p1, player * p2, player * p3, player * p4, player * p5)
-  {
-    p1 = *p1;
-    p2 = *p2;
-    p3 = *p3;
-    p4 = *p4;
-    p5 = *p5;
-  }
-  player playerObjects[] = { p1, p2, p3, p4, p5 };
+	bidding(player* p1, player* p2, player* p3, player* p4, player* p5)
+	{
+		playerObj[0] = p1;
+		playerObj[1] = p2;
+		playerObj[2] = p3;
+		playerObj[3] = p4;
+		playerObj[4] = p5;
+	}
 
-  string nameComparison ()
-  {
+	
+	string nameComparison()
+	{
+// create an  array and add all the amounts bidded by the players
+		int arr[] = { playerObj[0]->amount, playerObj[1]->amount,
+		  playerObj[2]->amount, playerObj[3]->amount,
+		  playerObj[4]->amount
+		};
+		cout << "\n" <<endl;
+		// prints the amount bid by all players
+		for (int i = 0; i < 5; i++) {
+			cout << playerObj[i]->firstName << " " << playerObj[i]->secondName << " bidded " << playerObj[i]->amount << " coins."<<endl;
+		}
+		//sort method will arrange the array of bidding numbers in descending to ascending order.
+		//the bidding number indexed 0 has the highest bidding number
 
-    int arr[] = { playerObjects[0]->amount, playerObjects[1]->amount,
-      playerObjects[2]->amount, playerObjects[3]->amount,
-      playerObjects[4]->amount
-    };
-    //sort method will arrange the array of bidding numbers in descending to ascending order.
-    //the bidding number indexed 0 has the highest bidding number
+		// now compare all the elements with arr[0] that has the highest bidding number 
+		//if bidding number is same or zero the compare method will compare the lastName.
+		int max = 0;
+		int counter = 0;
+		string winner;int winnerid=0; 
 
-    // now compare all the elements with arr[0] that has the highest bidding number 
-    //if bidding number is same or zero the compare method will compare the lastName.
-    int max = 0;
-    int counter = 0;
-    string winner;
+		for (int i = 0; i < 5; i++)
+		{
+			winningPlayer = playerObj[i];
+			for (int j = i; j < 5; j++)
+			{
+				player* runnerUp = playerObj[j];
+				if (playerObj[i]->amount <= playerObj[j]->amount)
+				{
+					if (max < playerObj[j]->amount)
+						{max = runnerUp->amount; winnerid = j;}
+				}
+			}
+		}
+		
+		int globalmin =0;
+		for (int i = 0; i < 5; i++)
+		{//for every single i compare it with j with same amount; the counter tell us
+			for (int j = i; j < 5; j++)
+			{
 
-    for (int i = 0; i < playerObjects.size (); i++)
-      {
-	winningPlayer = playerObjects[i];
-	for (int j = i; j < player.size (); j++)
-	  {
-	    player runnerUp = playerObjects[j];
-	    if (playerObjects[i]->amount <= playerObjects[j]->amount)
-	      {
-		if (max < playerObjects[j]->amount)
-		  max = runnerUp->amount;
-	      }
-	  }
-      }
-    for (int i = 0; i < playerObjects.size (); i++)
-      {				//for every single i compare it with j with same amount; the counter tell us
-	for (int j = 0; j < playerObjects.size (); j++)
-	  {			// considering that the amount is maximum and amount bid are same i.e tied,we compare their lastnames to realise
+				if (playerObj[i]->amount == max && playerObj[i]->amount == playerObj[j]->amount)
+				{
+				    int local = (playerObj[i]->secondName.
+                        compare(playerObj[j]->secondName));
+                    if (local<globalmin)
+                    {    
+                        cout << " bidded " << playerObj[j]->secondName <<endl;
+                        globalmin = local;
+                        winnerid = i;
+                        //winner = playerObj[i]->secondName;
+                    }
+				// 	if ((playerObj[i]->secondName.compare(playerObj[j]->secondName) < 0))
+				// 	{
+				// 	    //winner = playerObj[i]->secondName;
+				// 		winnerid = i;
+						
+						
+				// 	}
+				// 	else{
+				// 		//winner = playerObj[j]->secondName;
 
-	    if (playerObjects[i]->amount == max
-		&& playerObjects[i]->amount == playerObjects[j]->amount)
-	      {
-		if ((playerObjects[i]->lastname.
-		     compare (playerObjects[j]->lastname) < 0) winner =
-		    playerObjects[i]->lastname;
-		    else
-		    winner = playerObjects[j]->lastname;
-		    if ((playerObjects[0]->lastname.
-			 compare (playerObjects[j]->lastname)) == 0) winner =
-		    playerObjects[i]->lastname;}
-		    }
-		    }
-		    }
+				// 		winnerid = j;
+				// 	}
+				// 	if ((playerObj[i]->secondName.compare(playerObj[j]->secondName)) == 0) {
+				// 		//winner =playerObj[i]->secondName;
+				// 		winnerid = i;
+				// 	}
+				}
+			}
+		}
+		winner = playerObj[winnerid]->firstName +" "+ playerObj[winnerid]->secondName;
+		delete[] playerObj;
+		playerObj = NULL;
+		return (winner);
+	}
+	
+
+};
+
+//player playerObjects[] = { p1, p2, p3, p4, p5 };
 
 
 
-		    };
+		  
--- Part5/bidding.h
+++ Part5/bidding.h
@@ -1,16 +1,12 @@
-#include "player.cpp"
-#include "main.cpp"
-#include "bidding.h"
-class bidding
-{
-public:			// Access specifier
-  player p1;
-  player p2;
-  player p3;
-  player p4;
-  player p5;
-  player winningPlayer;		// Attributes
+#include <iostream>
+#include "player.h"
+#include <string>
+#include <algorithm>
+using namespace std;
+struct bidding {
+		
+			player *winningPlayer;	// Attribute
+			bidding()
 
-  bidding (player * p1, player * p2, player * p3, player * p4, player * p5)
-    string nameComparison ()};
-#endif
+			string nameComparison()
+};
