320e5cf4fc184a3d8db94c8bf03d14e5345872bf
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2021-03-28 17:54:13 -0400

    commit

--- Cards.h
+++ Cards.h
@@ -11,6 +11,7 @@ struct Card
     Card();
     Card(string t,string g,string a); 
     Card(const Card &C);
+    Card operator = (Card* obj);
     void show();
     string toString();
 };
@@ -32,6 +33,7 @@ class Deck
         void Show();
         Card* draw();    //returns the card at index curr_top
                         //and increments 
+        Deck operator = (Deck* obj);
         friend ostream& operator << (ostream& out, Deck& obj);
         ~Deck();
 };
@@ -56,6 +58,7 @@ class Hand : public Deck
                 // we call the method with index 4, the result would be
                 // faceup_cards:    Card1  Card2  Card3  Card4  Card6  Card19 
                 // since the next card would be picked from a shuffled deck    
+        Hand operator = (Hand* obj);
         friend ostream& operator << (ostream& out, Hand& obj);
         ~Hand();
 };
